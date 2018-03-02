6e5f50c71df961671645121038d2e9d8dc603a0d
= <osvaldo.capelani@gmail.com>
2018-03-02 16:50:25 -0400

    comecando a separar os aquivos

---
+++ paginas/carrossel.php
@@ -0,0 +1,61 @@
+<!-- Carousel
+    ================================================== -->
+<div class="container">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner" role="listbox">
+
+            <div class="item active">
+             <img class="first-slide" src="images/1.jpg" alt="First slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>EAF-Cuiabá 75 Anos!</h1>
+                        <p>Festa para egressos, aposentados e comunidade</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Inscreva-se</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img class="second-slide" src="images/2.jpg" alt="Second slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Nossa história</h1>
+                        <p>No começo era o início, mais tarde, com muito esforço veio o meio....</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Leia mais</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img class="third-slide" src="images/3.jpg" alt="Third slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>HOJE</h1>
+                        <p>O passado já foi, mas agora é hora de relembrar</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Veja as fotos</a></p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+
+    </div><!-- /.carousel -->
+
+</div>
